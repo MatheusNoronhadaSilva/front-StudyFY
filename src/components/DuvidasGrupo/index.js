0435600c5146dd45e0e6837c7885c68887abec16
RICARDO BORGES DO AMARAL <23111826@CFP127ALUNO.LOCAL>
2024-11-28 16:53:14 -0300

    atualizações no grupo aba duvida

--- src/components/DuvidasGrupo/index.js
+++ src/components/DuvidasGrupo/index.js
@@ -4,6 +4,7 @@ import questionIcon from '../../assets/question.png';
 import questionIconClicked from '../../assets/question (1).png';
 
 const AbaDuvidas = () => {
+  const [respostasVisiveis, setRespostasVisiveis] = useState({});
   const [duvidas, setDuvidas] = useState([]);
   const [showResposta, setShowResposta] = useState({}); // Estado para exibir a resposta de cada dúvida
   const [showForm, setShowForm] = useState(false);
@@ -24,17 +25,27 @@ const AbaDuvidas = () => {
 
   const fetchResposta = async (duvidaId) => {
     try {
-      const response = await fetch(`http://localhost:8080/v1/studyFy/respostas/${duvidaId}`);
+      const response = await fetch('http://localhost:8080/v1/studyFy/respostas');
       const data = await response.json();
-      if (data.resposta) {
-        return data.resposta.conteudo;
+
+      if (data.respostas) {
+        const respostasFiltradas = data.respostas.filter(
+          (resposta) => resposta.duvida_compartilhada_id === duvidaId
+        );
+        return respostasFiltradas.map((resposta) => ({
+          conteudo: resposta.conteudo,
+          mentor: `Mentor ${resposta.mentor_id}`, // Pode ajustar conforme o nome do mentor
+        }));
       }
-      return 'Nenhuma resposta disponível.';
+
+      return [];
     } catch (error) {
-      console.error('Erro ao buscar a resposta:', error);
-      return 'Erro ao carregar a resposta.';
+      console.error('Erro ao buscar as respostas:', error);
+      return [];
     }
   };
+
+
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -74,20 +85,22 @@ const AbaDuvidas = () => {
   };
 
   const toggleResposta = async (id_duvida) => {
-    // Verifica se a resposta já foi carregada
-    if (!showResposta[id_duvida]) {
-      const resposta = await fetchResposta(id_duvida); // Busca a resposta da dúvida
+    // Se a resposta já estiver visível para a dúvida clicada, fecha a resposta
+    if (showResposta[id_duvida]) {
       setShowResposta((prev) => ({
         ...prev,
-        [id_duvida]: resposta, // Armazena a resposta no estado
+        [id_duvida]: null, // Fecha a resposta atual
       }));
     } else {
-      setShowResposta((prev) => ({
-        ...prev,
-        [id_duvida]: null, // Remove a resposta do estado
-      }));
+      // Caso contrário, fecha todas as respostas e abre a nova
+      const resposta = await fetchResposta(id_duvida); // Busca a resposta
+      setShowResposta({
+        [id_duvida]: resposta, // Armazena a nova resposta
+      });
     }
   };
+  
+
 
   const handleIconClick = (id_duvida) => {
     setClickedIcons((prev) => {
@@ -122,127 +135,192 @@ const AbaDuvidas = () => {
       }}>Dúvidas</h3>
 
       <div style={{ width: '95%', padding: '10px' }}>
-        {duvidas.map((duvida) => (
-          <div key={duvida.id_duvida} style={{
+      {duvidas.map((duvida) => {
+  const temResposta = showResposta[duvida.id_duvida] && showResposta[duvida.id_duvida].length > 0; // Verifica se a dúvida tem resposta
+  return (
+    <div
+      key={duvida.id_duvida}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        width: '100%',
+        borderRadius: '8px',
+        padding: '10px',
+        backgroundColor: 'white',
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+        marginBottom: '10px',
+        position: 'relative',
+        border: temResposta ? '2px solid #FEE101' : 'none', // Aplica borda amarela se tiver resposta
+      }}
+    >
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+      }}>
+        <img
+          src={ftPerfil}
+          alt="Foto de perfil"
+          style={{
+            width: '50px',
+            height: '50px',
+            borderRadius: '50%',
+            marginRight: '10px',
+            marginLeft: '10px',
+          }}
+        />
+        <div style={{ flex: 1 }}>
+          <div style={{
             display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            width: '100%',
-            borderRadius: '8px',
-            padding: '10px',
-            backgroundColor: 'white',
-            boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-            marginBottom: '10px',
-            position: 'relative',
+            justifyContent: 'space-between',
+            alignItems: 'center',
           }}>
-            <div style={{
-              display: 'flex',
-              alignItems: 'center',
-              width: '100%',
-            }}>
-              <img 
-                src={ftPerfil} 
-                alt="Foto de perfil" 
-                style={{
-                  width: '50px',
-                  height: '50px',
-                  borderRadius: '50%',
-                  marginRight: '10px',
-                  marginLeft: '10px',
-                }} 
-              />
-              <div style={{ flex: 1 }}>
-                <div style={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                }}>
-                  <span style={{
-                    fontWeight: 'bold',
-                    fontSize: '1rem',
-                  }}>{duvida.nome_aluno || 'Aluno Anônimo'}</span>
-                  <span style={{
-                    fontSize: '0.8rem',
-                    color: '#666',
-                    marginBottom: '20px'
-                  }}>{duvida.data_envio}</span>
-                </div>
-                <p style={{
-                  fontSize: '0.9rem',
-                  color: '#333',
-                  marginTop: '5px',
-                }}>
-                  {duvida.conteudo_duvida || 'Sem conteúdo'}
-                </p>
-              </div>
-            </div>
-
-            {/* Ícone de Pergunta e Contador à direita */}
-            <div style={{ 
-              position: 'absolute',
-              top: '10px',
-              right: '10px',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-              <img 
-                src={clickedIcons[duvida.id_duvida] ? questionIconClicked : questionIcon}
-                alt="Ícone de Pergunta"
-                onClick={() => handleIconClick(duvida.id_duvida)}
-                style={{
-                  width: '20px', // Tamanho reduzido
-                  height: '20px', // Tamanho reduzido
-                  cursor: 'pointer',
-                  marginRight: '5px',
-                  marginTop: '35px' // Espaçamento à direita
-                }}
-              />
-              <span style={{
-                fontSize: '1.0rem',
-                color: '#666',
-                marginTop: '80px'
-              }}>
-                {clickedIcons[duvida.id_duvida] ? 1 : 0} {/* Exibe 1 se clicado, 0 se não */}
-              </span>
-            </div>
-
-            <button 
-              style={{
-                marginTop: '10px',
-                backgroundColor: '#FEE101',
-                color: 'black',
-                border: 'none',
-                padding: '4px 8px',
-                borderRadius: '4px',
-                cursor: 'pointer',
-                fontSize: '0.9rem',
-                boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
-              }}
-              onClick={() => toggleResposta(duvida.id_duvida)}
-            >
-              Ver Resposta
-            </button>
-
-            {showResposta[duvida.id_duvida] && (
-              <div style={{
-                marginTop: '10px',
-                padding: '10px',
-                borderRadius: '4px',
-                backgroundColor: '#f1f1f1',
-                color: '#333',
-                boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-              }}>
-                <strong>Resposta:</strong>
-                <p>{showResposta[duvida.id_duvida]}</p>
-              </div>
-            )}
+            <span style={{
+              fontWeight: 'bold',
+              fontSize: '1rem',
+            }}>{duvida.nome_aluno || 'Aluno Anônimo'}</span>
+            <span style={{
+              fontSize: '0.8rem',
+              color: '#666',
+              marginBottom: '20px'
+            }}>{duvida.data_envio}</span>
           </div>
-        ))}
+          <p style={{
+            fontSize: '0.9rem',
+            color: '#333',
+            marginTop: '5px',
+          }}>
+            {duvida.conteudo_duvida || 'Sem conteúdo'}
+          </p>
+        </div>
       </div>
 
+      {/* Ícone de Pergunta e Contador à direita */}
+      <div style={{
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <img
+          src={clickedIcons[duvida.id_duvida] ? questionIconClicked : questionIcon}
+          alt="Ícone de Pergunta"
+          onClick={() => handleIconClick(duvida.id_duvida)}
+          style={{
+            width: '20px', // Tamanho reduzido
+            height: '20px', // Tamanho reduzido
+            cursor: 'pointer',
+            marginRight: '5px',
+            marginTop: '35px' // Espaçamento à direita
+          }}
+        />
+        <span style={{
+          fontSize: '1.0rem',
+          color: '#666',
+          marginTop: '80px'
+        }}>
+          {clickedIcons[duvida.id_duvida] ? 1 : 0} {/* Exibe 1 se clicado, 0 se não */}
+        </span>
+      </div>
+
+      <button
+        style={{
+          marginTop: '10px',
+          backgroundColor: '#FEE101',
+          color: 'black',
+          border: 'none',
+          padding: '4px 8px',
+          borderRadius: '4px',
+          cursor: 'pointer',
+          fontSize: '0.9rem',
+          boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+        }}
+        onClick={() => toggleResposta(duvida.id_duvida)}
+      >
+        {showResposta[duvida.id_duvida] ? 'Ocultar Resposta' : 'Ver Resposta'}
+      </button>
+
+      {showResposta[duvida.id_duvida] && (
+        <div
+          style={{
+            position: 'fixed',
+            top: '25%',
+            left: '76%',
+            transform: 'translateX(-50%)',
+            backgroundColor: 'white',
+            border: '2px solid #FEE101', // Borda amarela
+            padding: '40px',
+            borderRadius: '8px',
+            boxShadow: '0px 4px 0px #FEE101', // Sombra
+            zIndex: 1000,
+            width: '70%',
+            maxWidth: '400px',
+          }}
+        >
+          {Object.keys(showResposta).map((duvidaId) => {
+            const respostas = showResposta[duvidaId];
+            return (
+              respostas &&
+              respostas.length > 0 && (
+                <div key={duvidaId}>
+                  <h5
+                    style={{
+                      fontSize: '1.5rem', // Aumenta o tamanho da fonte
+                      color: 'black', // Texto em amarelo
+                      marginBottom: '15px',
+                    }}
+                  >
+                    Respostas:
+                  </h5>
+                  {respostas.map((resposta, index) => (
+                    <div
+                      key={index}
+                      style={{
+                        marginBottom: '15px', // Espaçamento entre cards
+                        padding: '10px', // Aumenta o padding do card
+                        backgroundColor: '#f4f4f4',
+                        borderRadius: '8px',
+                        border: '2px solid #d9d9d9', // Adiciona borda amarela
+                        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)', // Adiciona sombra ao card
+                      }}
+                    >
+                      <p
+                        style={{
+                          fontWeight: 'bold',
+                          color: 'black', // Nome do mentor em amarelo
+                          fontSize: '1.2rem', // Aumenta o tamanho do texto
+                        }}
+                      >
+                        {resposta.mentor}
+                      </p>
+                      <p
+                        style={{
+                          fontSize: '1rem', // Tamanho da resposta
+                          color: '#333',
+                        }}
+                      >
+                        {resposta.conteudo}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              )
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+})}
+      </div>
+      {/* Card de Respostas fora do card de dúvida */}
+
+
       {/* Botão flutuante para adicionar uma nova dúvida */}
-      <button 
+      <button
         style={{
           position: 'fixed',
           top: '82%',
@@ -274,7 +352,7 @@ const AbaDuvidas = () => {
           backgroundColor: 'white',
           padding: '20px',
           borderRadius: '8px',
-          boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', // Aumentado para maior destaque
+          boxShadow: '0px 4px 0px #fee101', // Aumentado para maior destaque
           zIndex: 1000,
           width: '400px',
           maxHeight: 'auto',
@@ -282,10 +360,10 @@ const AbaDuvidas = () => {
         }}>
           <h3 style={{ color: '#333' }}>Adicionar nova dúvida</h3>
           <form onSubmit={handleFormSubmit}>
-            <textarea 
-              name="conteudo_duvida" 
-              value={newDuvida.conteudo_duvida} 
-              onChange={handleInputChange} 
+            <textarea
+              name="conteudo_duvida"
+              value={newDuvida.conteudo_duvida}
+              onChange={handleInputChange}
               placeholder="Digite sua dúvida..."
               rows="4"
               style={{
@@ -304,10 +382,10 @@ const AbaDuvidas = () => {
               justifyContent: 'space-between',
               alignItems: 'center',
             }}>
-              <button 
+              <button
                 type="submit"
                 style={{
-                  backgroundColor: '#FEE101', 
+                  backgroundColor: '#FEE101',
                   border: 'none',
                   padding: '6px 12px',
                   borderRadius: '4px',
@@ -317,9 +395,9 @@ const AbaDuvidas = () => {
               >
                 Enviar
               </button>
-              <button 
-                type="button" 
-                onClick={handleCancelClick} 
+              <button
+                type="button"
+                onClick={handleCancelClick}
                 style={{
                   backgroundColor: 'red',
                   border: 'none',
